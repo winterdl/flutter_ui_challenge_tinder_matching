9c4844be4c41b8f107aa557b261b773fae7dca52
Matt Carroll <me@mattcarroll.name>
2018-05-23 19:19:47 -0700

    Initial commit. Tinder Matching.

---
+++ README.md
@@ -0,0 +1,8 @@
+# prototype
+
+Flutter Challenge - Tinder Matching
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(''),
+      ),
+      body: new Center(),
+    );
+  }
+}
