217599a194a369d659112365a78928e8abbb0207
Matt Carroll <me@mattcarroll.name>
2018-05-31 23:03:06 -0700

    2. Static Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttery/layout.dart';
 
 void main() => runApp(new MyApp());
 
@@ -89,12 +90,114 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget _buildCardStack() {
+    return new AnchoredOverlay(
+      showOverlay: true,
+      child: new Center(),
+      overlayBuilder: (BuildContext context, Rect anchorBounds, Offset anchor) {
+        return CenterAbout(
+          position: anchor,
+          child: new Container(
+            width: anchorBounds.width,
+            height: anchorBounds.height,
+            padding: const EdgeInsets.all(16.0),
+            child: new ProfileCard(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: _buildAppBar(),
-      body: new Container(),
+      body: _buildCardStack(),
       bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  Widget _buildBackground() {
+    return new Image.asset(
+      'assets/photo_1.jpg',
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildProfileSynopsis() {
+    return new Positioned(
+      left: 0.0,
+      right: 0.0,
+      bottom: 0.0,
+      child: new Container(
+        decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.transparent,
+            Colors.black.withOpacity(0.8),
+          ],
+        )),
+        padding: const EdgeInsets.all(24.0),
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Text('First Last',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                      )),
+                  new Text('Some description',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      )),
+                ],
+              ),
+            ),
+            new Icon(
+              Icons.info,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10.0),
+        boxShadow: [
+          new BoxShadow(
+            color: const Color(0x11000000),
+            blurRadius: 5.0,
+            spreadRadius: 2.0,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: new BorderRadius.circular(10.0),
+        child: new Material(
+          child: new Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              _buildBackground(),
+              _buildProfileSynopsis(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
